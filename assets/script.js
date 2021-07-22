995a59db8a64279b060653bd7c6c014b85e6a1cf
artem.herasymiuk <vinewood02@gmail.com>
2021-07-22 17:34:40 +0300

    add js arrow into collapse button

--- assets/script.js
+++ assets/script.js
@@ -26,3 +26,9 @@ function includeHTML() {
       }
     }
   }
+  $('.arrow').click(function(){
+    let element = $('#arrow');
+    
+    (element.attr('class')=='fas fa-arrow-left') ? element.attr('class','fas fa-arrow-right'):element.attr('class','fas fa-arrow-left');
+    
+  })
